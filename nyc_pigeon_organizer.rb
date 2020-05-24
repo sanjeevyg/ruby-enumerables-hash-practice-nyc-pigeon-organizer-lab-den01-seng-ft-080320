75c29ed220fac4fc6d673796a825005918f4b49a
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-24 05:56:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  name_1 = []
+  data.each {|key, value|
+     name_1 << value
+  }
+  name_2 = []
+  count_1 = 0
+  while count_1 < name_1.length do
+    binding.pry
+    count_2 = 0
+    while count_2 < name_1[count_1].length do 
+      name_2 << name_1[count_1]
+      count_2 += 0
+    end 
+    count_1 += 1 
+  end 
+  p name_1
 end
+
+
